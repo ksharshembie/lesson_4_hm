4da68c9b0a8ae6f0b61f3bef7a8bda8ab41b82df
ksharshembie <kadyrbek.sharshembiev@gmail.com>
2022-02-05 21:01:48 +0600

    добавлен Medic, у которого есть способность лечить после каждого раунда на N-ное количество единиц здоровья только одного из членов команды, имеющего здоровье менее 100 единиц. Мертвых героев медик оживлять не может, и лечит он до тех пор пока жив сам. Медик не участвует в бою, но получает урон от Босса. Сам себя медик лечить не может.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,15 @@ import java.util.Random;
 
 public class Main {
 
-    public static int bossHealth = 600;
+    public static int bossHealth = 700;
     public static int bossDamage = 50;
     public static String bossDefenceType;
-    public static int[] heroesHealth = {260, 250, 270};
+    public static int[] heroesHealth = {260, 210, 270};
     public static int[] heroesDamage = {20, 15, 10};
     public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic"};
     public static int roundNumber;
+    public static int medicHealth = 300;
+    public static int medicTreatment;
 
     public static void main(String[] args) {
         printStatistics();
@@ -29,9 +31,11 @@ public class Main {
 
     public static void round() {
         roundNumber++;
+        System.out.println(roundNumber + " ROUND -------------");
         chooseBossDefence();
         bossHits();
         heroesHit();
+        setMedicTreament();
         printStatistics();
     }
 
@@ -45,12 +49,19 @@ public class Main {
                 }
             }
         }
+        if (bossHealth > 0 && medicHealth > 0) {
+            if (medicHealth - bossDamage < 0) {
+                medicHealth = 0;
+            } else {
+                medicHealth -= bossDamage;
+            }
+        }
     }
 
     public static void heroesHit() {
         for (int i = 0; i < heroesDamage.length; i++) {
             if (bossHealth > 0 && heroesHealth[i] > 0) {
-                if (bossDefenceType == heroesAttackType[i]) {
+                if (bossDefenceType.equals(heroesAttackType[i])) {
                     Random random = new Random();
                     int coeff = random.nextInt(9) + 2; // 2,3,4,5,6,7,8,9,10
                     if (bossHealth - heroesDamage[i] * coeff < 0) {
@@ -72,33 +83,62 @@ public class Main {
 
     public static boolean isGameFinished() {
         if (bossHealth <= 0) {
-            System.out.println("Heroes won!!!");
+            System.out.println("\n Heroes won!!!");
             return true;
         }
-        /*if (heroesHealth[0] <= 0 && heroesHealth[1] <= 0 && heroesHealth[2] <= 0) {
-            System.out.println("Boss won!!!");
-            return true;
-        }
-        return false;*/
         boolean allHeroesDead = true;
-        for (int i = 0; i < heroesHealth.length; i++) {
-            if (heroesHealth[i] > 0) {
+        for (int health : heroesHealth) {
+            if (health > 0) {
                 allHeroesDead = false;
                 break;
             }
         }
         if (allHeroesDead) {
-            System.out.println("Boss won!!!");
+            System.out.println("\n Boss won!!!");
         }
         return allHeroesDead;
     }
 
     public static void printStatistics() {
-        System.out.println(roundNumber + " ROUND -------------");
         System.out.println("Boss health: " + bossHealth + " (" + bossDamage + ")");
         for (int i = 0; i < heroesHealth.length; i++) {
-            System.out.println(heroesAttackType[i] + " health: "
-                    + heroesHealth[i] + " (" + heroesDamage[i] + ")");
+            if (heroesHealth[i] > 0) {
+                System.out.println(heroesAttackType[i] + " health: "
+                        + heroesHealth[i] + " (" + heroesDamage[i] + ")");
+            } else {
+                System.out.println(heroesAttackType[i] + " health: "
+                        + heroesHealth[i] + " (" + 0 + ")");
+            }
+        }
+        if (medicHealth > 0) {
+            System.out.println("Medic health: " + medicHealth + " (" + medicTreatment + ")");
+        } else {
+            System.out.println("Medic health: " + medicHealth + " (" + 0 + ")");
+        }
+
+    }
+
+    public static void setMedicTreament() {
+        Random random = new Random();
+        boolean isTreated = false;
+        boolean isTreatNeeds = false;
+        for (int health : heroesHealth) {
+            if (health > 0 && health < 100) {
+                isTreatNeeds = true;
+                break;
+            }
+        }
+        if (medicHealth > 0 && isTreatNeeds) {
+            while (!isTreated) {
+                int i = random.nextInt(heroesHealth.length);
+                if (heroesHealth[i] > 0 && heroesHealth[i] < 100) {
+                    medicTreatment = random.nextInt(11) * 10;
+                    System.out.println(heroesAttackType[i] + " health before Treatment: " + heroesHealth[i]);
+                    heroesHealth[i] += medicTreatment;
+                    isTreated = true;
+                } else medicTreatment = 0;
+            }
         }
     }
 }
+
